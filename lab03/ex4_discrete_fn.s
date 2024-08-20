ee7108761a93ec540e7eaeb94d84dae76d6d2c12
Hanzhen <zhouhanzhen2023@gmail.com>
2024-08-20 16:52:27 +0800

    Complete lab04

--- lab03/ex4_discrete_fn.s
+++ lab03/ex4_discrete_fn.s
@@ -6,6 +6,17 @@
 # The return value should be stored in a0
 f:
     # Your code here
+    # first argument in a0 is the input, second argument in a1 is the address of output array
+
+    addi t0 a0 3    # get the index of input  
+    mv t1 a1        # store the pointer of the array in t1
+
+    slli t2 t0 2    # multiply the index by the size of int to get the offset
+    add t2 t2 t1    # add this offset to the address of the array
+    lw t3 0(t2)     # read the element
+
+    mv a0 t3        # put the element in a0 for returned value
+
 
     # This is how you return from a function. You'll learn more about this later.
     # This should be the last line in your program.
--- lab03/ex5_factorial.s
+++ lab03/ex5_factorial.s
@@ -28,6 +28,29 @@ main:
 factorial:
     # YOUR CODE HERE
 
+    mv t0 a0            # get the value of n
+    addi t1 x0 1        # initialize the vaule of i; i = 1
+    addi t4 x0 1        # initialize the vaule of return value, re = 1
+
+
+outerloop:
+    addi t2 x0 1        # initialize the value of j; j = 1
+    add t3 x0 x0        # initialize the value of sum ; sum = 0
+
+
+innerloop: 
+    add t3 t3 t4        # sum += re
+    addi t2 t2 1        # j += 1
+    bge t1 t2 innerloop # branch if i >= j
+
+
+    mv t4 t3            # re = sum
+    addi t1 t1 1        # i += 1
+    bge t0 t1 outerloop # branch if n >= i
+
+    mv a0 t4            # put the result in a0
+
+
     # This is how you return from a function. You'll learn more about this later.
     # This should be the last line in your program.
     jr ra
